135446cbfa5f9e1972405256326b94365455b287
vaibhavi <vaibhavikardale@gmail.com>
2019-03-08 00:50:07 +0530

    R - objects

--- Basics.R
+++
@@ -1,4 +0,0 @@
-# R has 5 atomic classes of Objects -- char, numeric ,int, complex, logical(T/F)
-# most basic object is a vector 
-# A vector can contains object of same class [except list which can contain multiple types]
-# Numbers - for int 1L (explicit), Inf - special val for infinity , Nan - Not a number
